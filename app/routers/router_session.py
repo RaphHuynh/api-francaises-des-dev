46baf83ce94bdd8d05eb7841dca9ce0ab7fae493
Jason YT <109391326+jasonviipers@users.noreply.github.com>
2023-11-03 01:48:51 +0100

    Update router_session.py
    Ajouter des documents et des commentaires significatifs pour plus de clarté.
    Gérer des exceptions spécifiques au lieu d'une exception générique.
    Utiliser HTTPException de FastAPI pour lever les codes d'état appropriés avec des messages significatifs.
    Regrouper et organiser les importations.

--- app/routers/router_session.py
+++ app/routers/router_session.py
@@ -3,12 +3,11 @@ from __future__ import annotations
 from typing import Optional, Union, Annotated
 
 import jwt
-from fastapi import APIRouter, Cookie, Header
+from fastapi import APIRouter, Cookie, Header, HTTPException
 from starlette.requests import Request
-
-from app.lib.sql import verif_session
 from starlette.responses import Response
 
+from app.lib.sql import verif_session
 from app.models.session import SessionCookie
 from app.settings import SECRET_KEY, ALGORITHM
 
@@ -17,17 +16,34 @@ router = APIRouter(
     tags=["session"]
 )
 
-
 @router.get("/")
-async def api_is_connected(request: Request):
+async def api_is_connected(request: Request) -> dict:
+    """
+    Verifies if a session is connected based on the 'access_token' cookie.
+
+    Args:
+        request: The incoming request object.
+
+    Returns:
+        dict: A dictionary containing a status code.
+    """
+    access_token = request.cookies.get('access_token')
+
+    # Check if access_token exists
+    if not access_token:
+        raise HTTPException(status_code=401, detail="Access token not found")
+
     try:
-        access_token = request.cookies.get('access_token')
         cookie_session_decode = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
         print(cookie_session_decode)
+
         if await verif_session(cookie_session_decode):
             return {"status": 200}
         else:
             return {"status": 401}
+
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid access token")
     except Exception as e:
         print(e)
-        return {"status": 400}
+        raise HTTPException(status_code=400, detail="An error occurred while verifying the session")
